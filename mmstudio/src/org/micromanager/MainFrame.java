kasbah/micromanager

commit c81842ae1e81d3fc7205473d3cd2f68f6e4e36ca

Fix for ticket #717.
Fixes a bug introduced by the MMStudio/MainFrame split that caused the "sync
main frame and MDA exposure times" option to be one-way only.

git-svn-id: https://valelab4.ucsf.edu/svn/micromanager2/trunk@14402 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

mmstudio/src/org/micromanager/MainFrame.java
@@ -240,14 +240,14 @@ public class MainFrame extends JFrame implements LiveModeListener {
       textFieldExp_.addFocusListener(new FocusAdapter() {
          @Override
          public void focusLost(FocusEvent fe) {
-            studio_.setExposure();
+            studio_.setExposure(Double.parseDouble(textFieldExp_.getText()));
          }
       });
       textFieldExp_.setFont(defaultFont_);
       textFieldExp_.addActionListener(new ActionListener() {
          @Override
          public void actionPerformed(ActionEvent e) {
-            studio_.setExposure();
+            studio_.setExposure(Double.parseDouble(textFieldExp_.getText()));
          }
       });
       GUIUtils.addWithEdges(topPanel, textFieldExp_, 203, 21, 276, 40);

mmstudio/src/org/micromanager/MMStudio.java
@@ -679,7 +679,7 @@ public class MMStudio implements ScriptInterface {
       contrastPrefs_.getInt("ContrastHistDisplayMode_" + channelGroup + "_" + type + channel, 1) );
    }
 
-   public void setExposure() {
+   public void setExposure(double exposureTime) {
       // This is synchronized with the shutdown lock primarily so that
       // the exposure-time field in MainFrame won't cause issues when it loses
       // focus during shutdown.
@@ -688,7 +688,7 @@ public class MMStudio implements ScriptInterface {
             // Just give up.
             return;
          }
-         snapLiveManager_.safeSetCoreExposure(frame_.getDisplayedExposureTime());
+         snapLiveManager_.safeSetCoreExposure(exposureTime);
          // Display the new exposure time
          double exposure;
          try {
@@ -1962,7 +1962,7 @@ public class MMStudio implements ScriptInterface {
          if (channelGroup != null && channelGroup.equals(core_.getChannelGroup())) {
             if (channel != null && !channel.equals("") && 
                     channel.equals(core_.getCurrentConfigFromCache(channelGroup))) {
-               setExposure();
+               setExposure(exposure);
             }
          }
       } catch (Exception ex) {

commit 22c35d555f9209556211e5ea2d4b66216c5c9999

Fix Exposure field for locales using comma as decimal point
Fix bug introduced in r14402.

git-svn-id: https://valelab4.ucsf.edu/svn/micromanager2/trunk@14415 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

mmstudio/src/org/micromanager/MainFrame.java
@@ -240,14 +240,14 @@ public class MainFrame extends JFrame implements LiveModeListener {
       textFieldExp_.addFocusListener(new FocusAdapter() {
          @Override
          public void focusLost(FocusEvent fe) {
-            studio_.setExposure(Double.parseDouble(textFieldExp_.getText()));
+            studio_.setExposure(getDisplayedExposureTime());
          }
       });
       textFieldExp_.setFont(defaultFont_);
       textFieldExp_.addActionListener(new ActionListener() {
          @Override
          public void actionPerformed(ActionEvent e) {
-            studio_.setExposure(Double.parseDouble(textFieldExp_.getText()));
+            studio_.setExposure(getDisplayedExposureTime());
          }
       });
       GUIUtils.addWithEdges(topPanel, textFieldExp_, 203, 21, 276, 40);