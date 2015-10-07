kasbah/micromanager

commit 2bf79c8f8a9c90f45a429e434b3229e95567fc20

ASIdiSPIM: clean up joystick code, some joystick names changed so users might notice their settings don't get restored, also switching tabs is now faster because we only unset the set joystick/wheel associations instead of needing to clear all associations
git-svn-id: https://valelab4.ucsf.edu/svn/micromanager2/trunk@15759 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

plugins/ASIdiSPIM/src/org/micromanager/asidispim/AcquisitionPanel.java
@@ -26,7 +26,6 @@ import org.micromanager.asidispim.Data.AcquisitionModes;
 import org.micromanager.asidispim.Data.CameraModes;
 import org.micromanager.asidispim.Data.Cameras;
 import org.micromanager.asidispim.Data.Devices;
-import org.micromanager.asidispim.Data.Joystick;
 import org.micromanager.asidispim.Data.MultichannelModes;
 import org.micromanager.asidispim.Data.MyStrings;
 import org.micromanager.asidispim.Data.Positions;
@@ -122,7 +121,6 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
 
    private final Devices devices_;
    private final Properties props_;
-   private final Joystick joystick_;
    private final Cameras cameras_;
    private final Prefs prefs_;
    private final ControllerUtils controller_;
@@ -187,7 +185,6 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
    public AcquisitionPanel(ScriptInterface gui, 
            Devices devices, 
            Properties props, 
-           Joystick joystick,
            Cameras cameras, 
            Prefs prefs,
            StagePositionUpdater posUpdater,
@@ -202,7 +199,6 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
       gui_ = gui;
       devices_ = devices;
       props_ = props;
-      joystick_ = joystick;
       cameras_ = cameras;
       prefs_ = prefs;
       posUpdater_ = posUpdater;
@@ -711,9 +707,6 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
          @Override
          public void actionPerformed(ActionEvent e) {
             updateJoysticks();
-            if (!navigationJoysticksCB_.isSelected()) {
-               joystick_.unsetAllJoysticks();
-            }
             prefs_.putBoolean(panelName_, Properties.Keys.PLUGIN_USE_NAVIGATION_JOYSTICKS,
                   navigationJoysticksCB_.isSelected());
          }
@@ -780,13 +773,10 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
    }//end constructor
    
    private void updateJoysticks() {
-      if (navigationJoysticksCB_.isSelected()) {
-         if (ASIdiSPIM.getFrame() != null) {
-            ASIdiSPIM.getFrame().getNavigationPanel().doJoystickSettings();
-         }
+      if (ASIdiSPIM.getFrame() != null) {
+         ASIdiSPIM.getFrame().getNavigationPanel().
+         doJoystickSettings(navigationJoysticksCB_.isSelected());
       }
-      // unsetAllJoysticks() should have been called when leaving last tab
-      // so no need to do it again now
    }
    
    public final void updateDurationLabels() {
@@ -1512,7 +1502,7 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
             MyDialogUtils.showError("\"Channels\" is checked, but no channels are selected");
             return false;
          }
-         // get current channel so that we can restore it
+         // get current channel so that we can restore it, then set channel appropriately
          originalChannelConfig = multiChannelPanel_.getCurrentConfig();
          switch (channelMode) {
          case VOLUME:
@@ -2263,7 +2253,11 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
       // TODO figure out why posUpdater_ is paused and then unpaused here
       posUpdater_.pauseUpdates(true);
       props_.callListeners();
-      updateJoysticks();
+      // old joystick associations were cleared when leaving
+      //   last tab so only do it if joystick settings need to be applied
+      if (navigationJoysticksCB_.isSelected()) {
+         updateJoysticks();
+      }
       sliceFrameAdvanced_.setVisible(advancedSliceTimingCB_.isSelected());
       posUpdater_.pauseUpdates(false);
    }

plugins/ASIdiSPIM/src/org/micromanager/asidispim/ASIdiSPIMFrame.java
@@ -63,8 +63,7 @@ import org.micromanager.utils.MMFrame;
 //TODO make it easy to look through series of data sets
 //TODO hardware Z-projection
 //TODO camera control tab: set ROI, set separate acquisition/alignment exposure times and sweep rate, etc.
-//TODO track Z/F for sample finding
-//TODO Z/F position dropdown for often-used positions
+//TODO method to move sample diagonally (move X and F at same time) for sample finding
 //TODO factor out common code for JComboBoxes like MulticolorModes, CameraModes, AcquisitionModes, etc.
 //TODO cleanup prefs vs. props... maybe add boolean support for plugin device use only?
 //TODO finish eliminating Prefs.Keys in favor of Properties.Keys with plugin values
@@ -72,6 +71,10 @@ import org.micromanager.utils.MMFrame;
 //TODO improve efficiency of camera code by pre-calculating key factors and updating when needed instead of calculating every time
 //TODO add check for correct Hamamatsu model
 //TODO execute autofocus during acquisition before the desired time point is reached instead of waiting until a timepoint should be collected
+//       or else do autofocus after acquisition instead of before
+//TODO smart default joystick settings (e.g. different defaults different panels/wheels)
+//TODO buttons to easily swap cameras within plugin
+//TODO easily take "test acquisition" that wouldn't need to be saved, only do 1 timepoint, etc. From both acquisition and setup tabs (setup tab would only do that side)
 
 
 /**
@@ -130,8 +133,8 @@ public class ASIdiSPIMFrame extends MMFrame
       autofocus_ = new AutofocusUtils(gui, devices_, props_, prefs_,
             cameras_, stagePosUpdater_, positions_, controller_);
       
-      acquisitionPanel_ = new AcquisitionPanel(gui, devices_, props_, joystick_, 
-            cameras_, prefs_, stagePosUpdater_, positions_, controller_, autofocus_);
+      acquisitionPanel_ = new AcquisitionPanel(gui, devices_, props_, cameras_, 
+            prefs_, stagePosUpdater_, positions_, controller_, autofocus_);
       setupPanelA_ = new SetupPanel(gui, devices_, props_, joystick_, 
             Devices.Sides.A, positions_, cameras_, prefs_, stagePosUpdater_,
             autofocus_);

plugins/ASIdiSPIM/src/org/micromanager/asidispim/Data/Devices.java
@@ -477,8 +477,8 @@ public class Devices {
       case GALVOA:
       case GALVOB:
          switch (dir) {
-         case X: ret += ", sheet"; break;
-         case Y: ret += ", slice"; break;
+         case X: ret += " Sheet"; break;
+         case Y: ret += " Slice"; break;
          default: break;
          }
          break;
@@ -519,9 +519,9 @@ public class Devices {
       case GALVOA:
       case GALVOB:
         if (side == d.side) {
-           ret = "Imaging Beam";
+           ret = "Imaging";
         } else if (side == Devices.getOppositeSide(d.side)) {
-           ret = "Epi Beam";
+           ret = "Epi";
         } else {
            ret = getDeviceDisplay(key);
         }

plugins/ASIdiSPIM/src/org/micromanager/asidispim/Data/Joystick.java
@@ -134,6 +134,11 @@ public class Joystick {
          }
          return this.direction == other.direction;
       }
+      
+      @Override
+      public String toString() {
+         return displayString;
+      }
    }
    
    /**
@@ -152,13 +157,11 @@ public class Joystick {
          }
       }
       // don't include the XY stage on wheel list since the device adapter doesn't support that
+      // only add the slice position, not sheet position because we don't expect it to ever be used
       for (Devices.Keys devKey : Devices.GALVOS) {
          // must be Tiger devices
-         Directions[] dirs = {Directions.X, Directions.Y};
-         for (Directions dir : dirs) {  // gets for X and Y for now
-            String dispKey = devices_.getDeviceDisplayWithRole(devKey, dir, side);
-            list.add(new JSAxisData(dispKey, devKey, dir));
-         }
+         String dispKey = devices_.getDeviceDisplayWithRole(devKey, Directions.Y, side);
+         list.add(new JSAxisData(dispKey, devKey, Directions.Y));
       }
       List<JSAxisData> noduplicates = new ArrayList<JSAxisData>(new LinkedHashSet<JSAxisData>(list));
       return noduplicates.toArray(new JSAxisData[0]);
@@ -244,8 +247,11 @@ public class Joystick {
     * @param JSAxisData
     */
    public void unsetJoystick(Joystick.Keys jkey, JSAxisData JSAxisData) {
+      if (JSAxisData == null) {
+         return;
+      }
       Devices.Keys dev = JSAxisData.deviceKey;
-      if (!devices_.isTigerDevice(dev)) {
+      if (!devices_.isTigerDevice(dev)) {  // includes "None"
          return;
       }
       try {

plugins/ASIdiSPIM/src/org/micromanager/asidispim/JoystickSubPanel.java
@@ -25,7 +25,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.util.HashMap;
 
 import javax.swing.BorderFactory;
 import javax.swing.JComboBox;
@@ -33,10 +32,13 @@ import javax.swing.JLabel;
 
 import org.micromanager.asidispim.Data.Devices;
 import org.micromanager.asidispim.Data.Joystick;
+import org.micromanager.asidispim.Data.Joystick.Directions;
+import org.micromanager.asidispim.Data.Joystick.JSAxisData;
 import org.micromanager.asidispim.Data.MyStrings;
 import org.micromanager.asidispim.Data.Prefs;
 import org.micromanager.asidispim.Utils.DevicesListenerInterface;
 import org.micromanager.asidispim.Utils.ListeningJPanel;
+import org.micromanager.asidispim.Utils.MyDialogUtils;
 
 import net.miginfocom.swing.MigLayout;
 
@@ -94,8 +96,7 @@ public final class JoystickSubPanel extends ListeningJPanel {
       add(leftWheelBox_, "wrap");
       
       add(new JLabel(Joystick.Keys.RIGHT_WHEEL.toString() + ":"));
-      rightWheelBox_ = makeJoystickSelectionBox(
-            Joystick.Keys.RIGHT_WHEEL);
+      rightWheelBox_ = makeJoystickSelectionBox(Joystick.Keys.RIGHT_WHEEL);
       add(rightWheelBox_, "wrap");
    }
    
@@ -106,9 +107,16 @@ public final class JoystickSubPanel extends ListeningJPanel {
       jcb.addItemListener(ssbl);
       devices_.addListener(ssbl);
       // intentionally set from prefs after adding listeners so programmatic change from prefs will be handled like user change
-      String selectedItem = prefs_.getString(instanceLabel_, jkey.toString(),
-            Joystick.Keys.NONE.toString());
-      jcb.setSelectedItem(selectedItem);
+      // we store string in prefs but have associative object as combo box items
+      // could simplify the equals method of JSAxisData to only look at the display string but afraid of unintended consequences
+      // so we look through combo box strings and select a match
+      String selectedItem = prefs_.getString(instanceLabel_, jkey.toString(), Joystick.Keys.NONE.toString());
+      for (int index = 0; index < jcb.getItemCount(); index++) {
+         if (jcb.getItemAt(index).toString().equals(selectedItem)) {
+            jcb.setSelectedIndex(index);
+            break;
+         }
+      }
       return jcb;
    }
    
@@ -118,14 +126,12 @@ public final class JoystickSubPanel extends ListeningJPanel {
    private class StageSelectionBoxListener implements ItemListener,
    ActionListener, DevicesListenerInterface {
       Joystick.Keys jkey_;
-      JComboBox jc_;
-      HashMap<String, Joystick.JSAxisData> JSAxisDataHash_;
+      JComboBox cb_;
       boolean updatingList_;
 
       public StageSelectionBoxListener(Joystick.Keys jkey, JComboBox jc) {
          jkey_ = jkey;
-         jc_ = jc;
-         JSAxisDataHash_ = new HashMap<String, Joystick.JSAxisData>();
+         cb_ = jc;
          this.updateStageSelections();  // do initial rendering
       }    
 
@@ -141,16 +147,18 @@ public final class JoystickSubPanel extends ListeningJPanel {
          if (updatingList_ == true) {
             return;  // don't go through this if we are rebuilding selections
          }
-         Joystick.JSAxisData axis = JSAxisDataHash_.get( (String) jc_.getSelectedItem());
-         if (axis != null) {
-            if (axis.deviceKey != Devices.Keys.NONE) {
-               joystick_.setJoystick(jkey_, axis);
-            }
-            else {
-               joystick_.unsetJoystick(jkey_, axis);
-            }
-            prefs_.putString(instanceLabel_, jkey_.toString(), (String) jc_.getSelectedItem());
+         Joystick.JSAxisData axisData = (JSAxisData) cb_.getSelectedItem();
+         if (axisData == null) {
+            MyDialogUtils.showError("Problem getting joystick value for joystick combobox.");
+            return;
          }
+         if (axisData.deviceKey != Devices.Keys.NONE) {
+            joystick_.setJoystick(jkey_, axisData);
+         }
+         else {
+            joystick_.unsetJoystick(jkey_, axisData);
+         }
+         prefs_.putString(instanceLabel_, jkey_.toString(), ((JSAxisData) cb_.getSelectedItem()).toString());
       }
       
       // have both actionlistener and itemlistener because need to do deselect operation
@@ -160,10 +168,8 @@ public final class JoystickSubPanel extends ListeningJPanel {
             return;  // don't go through this if we are rebuilding selections
          }
          if (e.getStateChange() == ItemEvent.DESELECTED) {  // clear the old association
-            Joystick.JSAxisData axis = JSAxisDataHash_.get( (String) e.getItem());  // gets deselected item
-            if (axis.deviceKey != Devices.Keys.NONE) {
-               joystick_.unsetJoystick(jkey_, axis);
-            }
+            Joystick.JSAxisData axisData = (JSAxisData) e.getItem();  // gets deselected item
+            joystick_.unsetJoystick(jkey_, axisData);
          }
       }
       
@@ -181,28 +187,35 @@ public final class JoystickSubPanel extends ListeningJPanel {
        */
       private void updateStageSelections() {
          // save the existing selection if it exists
-         String itemOrig = (String) jc_.getSelectedItem();
+         // if we are making combobox for first time itemOrig will be null; we set
+         // it later from the preference value in makeJoystickSelectionBox()
+         JSAxisData itemOrig = (JSAxisData) cb_.getSelectedItem();
+         if (itemOrig == null) {
+            itemOrig = new JSAxisData(devices_.getDeviceDisplayVerbose(Devices.Keys.NONE), Devices.Keys.NONE, Directions.NONE);
+         }
          joystick_.unsetAllDevicesFromJoystick(jkey_);
          
          // get the appropriate list of strings (in form of JSAxisData array) depending on joystick device type
+         // already includes "None" at the top of each list
          Joystick.JSAxisData[] JSAxisDataItems = null;
-         JSAxisDataHash_.clear();
          if (jkey_==Joystick.Keys.LEFT_WHEEL || jkey_==Joystick.Keys.RIGHT_WHEEL) {
             JSAxisDataItems = joystick_.getWheelJSAxisData(side_);
          }
          else if (jkey_==Joystick.Keys.JOYSTICK) {
             JSAxisDataItems = joystick_.getStickJSAxisData(side_);
          }
+         else {
+            MyDialogUtils.showError("Unknown joystick device type.");
+            return;
+         }
          
          // repopulate the combo box with items
          updatingList_ = true;  // make sure itemStateChanged isn't fired until we rebuild list
          boolean itemInNew = false;
-         jc_.removeAllItems();
-         for (Joystick.JSAxisData a : JSAxisDataItems) {
-            String s = a.displayString;
-            jc_.addItem(s);
-            JSAxisDataHash_.put(s, a);
-            if (s.equals(itemOrig)) {
+         cb_.removeAllItems();
+         for (Joystick.JSAxisData item : JSAxisDataItems) {
+            cb_.addItem(item);
+            if (item.equals(itemOrig)) {
                itemInNew = true;
             }
          }
@@ -210,10 +223,11 @@ public final class JoystickSubPanel extends ListeningJPanel {
          
          // restore the original selection if it's still present
          if (itemInNew) {
-            jc_.setSelectedItem(itemOrig);
+            cb_.setSelectedItem(itemOrig);
          }
          else {
-            jc_.setSelectedItem(Joystick.Keys.NONE.toString());
+            cb_.setSelectedItem(new JSAxisData(devices_.getDeviceDisplayVerbose(Devices.Keys.NONE),
+                  Devices.Keys.NONE, Directions.NONE));
          }
       }//updateStageSelections
 
@@ -225,9 +239,9 @@ public final class JoystickSubPanel extends ListeningJPanel {
     */
    @Override
    public void gotSelected() {
-      joystickBox_.setSelectedItem(joystickBox_.getSelectedItem());
-      rightWheelBox_.setSelectedItem(rightWheelBox_.getSelectedItem());      
-      leftWheelBox_.setSelectedItem(leftWheelBox_.getSelectedItem());
+      joystickBox_.setSelectedItem((JSAxisData) joystickBox_.getSelectedItem());
+      rightWheelBox_.setSelectedItem((JSAxisData) rightWheelBox_.getSelectedItem());      
+      leftWheelBox_.setSelectedItem((JSAxisData) leftWheelBox_.getSelectedItem());
    }
 
    /**
@@ -235,13 +249,9 @@ public final class JoystickSubPanel extends ListeningJPanel {
     */
    @Override
    public void gotDeSelected() {
-      // TODO change to using joystick_.unsetJoystick() for relevant associations
-      // need to change internal representation of ComboBox to using objects instead of strings
-      // to store associations, then this will be easy
-      // also could use JSAxisDataHash_
-      // alternatively could send single long command which will unset all joysticks
-      // but then would need to keep track of all axes
-      joystick_.unsetAllJoysticks();
+      joystick_.unsetJoystick(Joystick.Keys.JOYSTICK, (JSAxisData) joystickBox_.getSelectedItem());
+      joystick_.unsetJoystick(Joystick.Keys.RIGHT_WHEEL, (JSAxisData) rightWheelBox_.getSelectedItem());
+      joystick_.unsetJoystick(Joystick.Keys.LEFT_WHEEL, (JSAxisData) leftWheelBox_.getSelectedItem());
    }
 
 }

plugins/ASIdiSPIM/src/org/micromanager/asidispim/NavigationPanel.java
@@ -527,7 +527,11 @@ public class NavigationPanel extends ListeningJPanel implements LiveModeListener
    /**
     * created so that Navigation panel's joystick settings could be invoked from elsewhere
     */
-   public void doJoystickSettings() {
-      joystickPanel_.gotSelected();
+   public void doJoystickSettings(boolean selected) {
+      if (selected) {
+         joystickPanel_.gotSelected();
+      } else {
+         joystickPanel_.gotDeSelected();
+      }
    }
 }

commit 12e8b6ab4dfe96b004338d7bddb0acabf9a4a1e1

ASIdiSPIM: fix bug where joystick settings weren't cleared on leaving acquisition panel (if they were being used), bug introduced in r15759
git-svn-id: https://valelab4.ucsf.edu/svn/micromanager2/trunk@15802 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

plugins/ASIdiSPIM/src/org/micromanager/asidispim/AcquisitionPanel.java
@@ -2296,6 +2296,12 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
     */
    @Override
    public void gotDeSelected() {
+      // if we have been using navigation panel's joysticks need to unset them
+      if (navigationJoysticksCB_.isSelected()) {
+         if (ASIdiSPIM.getFrame() != null) {
+            ASIdiSPIM.getFrame().getNavigationPanel().doJoystickSettings(false);
+         }
+      }
       sliceFrameAdvanced_.setVisible(false);
       saveSettings();
    }