kasbah/micromanager

commit 13fa78b25c617bd371fe5c7b04dd72fd2f0bfd66

diSPIM plugin: Incorporating Autofocus into the Acquisition.  Autofocus will run, but not yet update the piezo/slice relation.  Also, the acquisition itself no longer runs correctly after autofocus.  This needs work!
git-svn-id: https://valelab4.ucsf.edu/svn/micromanager2/trunk@15471 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

plugins/ASIdiSPIM/src/org/micromanager/asidispim/Utils/StagePositionUpdater.java
@@ -44,6 +44,7 @@ public class StagePositionUpdater {
    private final AtomicBoolean updatingNow_ = new AtomicBoolean(false);  // true iff in middle of update right now, use to skip updates if last one is running instead of letting them pile up
    private final AtomicBoolean pauseUpdates_ = new AtomicBoolean(false);  // true iff updates temporarily disabled (e.g. running acquisition)
    private boolean timerRunning; // whether we are set to update positions currently
+   private int pauseCounter_ = 0;
    
    /**
     * Utility class for stage position timer.
@@ -140,13 +141,28 @@ public class StagePositionUpdater {
    
    /**
     * Call this with true to temporarily turn off updates.
-    * Be sure to call it again with false.
-    * @param pause true disables updates temporarily, false goes back to normal
+    * Be sure to call it again with false. Keeps an internal counter
+    * so that the calling code does not need to know whether other code
+    * also called the pauseUpdate function.
+    * @param pause true disables updates temporarily, and increases the pause 
+    * counter.  false decreases the pause counter.  When the counter returns to 
+    * 0, unpauses the updater
     */
    public void pauseUpdates(boolean pause) {
-      pauseUpdates_.set(pause);
-      for (ListeningJPanel panel : panels_) {
-         panel.stoppedStagePositions();
+      if (pause) {
+         pauseCounter_++;
+      } else {
+         pauseCounter_--;
+         // protect against the counter going negative:
+         if (pauseCounter_ < 0) {
+            pauseCounter_ = 0;
+         }
+      }
+      if (pauseCounter_ < 2) {
+         pauseUpdates_.set(pause);
+         for (ListeningJPanel panel : panels_) {
+            panel.stoppedStagePositions();
+         }
       }
    }
    

plugins/ASIdiSPIM/src/org/micromanager/asidispim/AcquisitionPanel.java
@@ -105,6 +105,7 @@ import java.awt.geom.Point2D;
 import javax.swing.BorderFactory;
 
 import org.micromanager.asidispim.Data.ChannelSpec;
+import org.micromanager.asidispim.Data.Devices.Sides;
 import org.micromanager.asidispim.Utils.ControllerUtils;
 import org.micromanager.asidispim.Utils.AutofocusUtils;
 
@@ -171,6 +172,7 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
    private final JCheckBox usePositionsCB_;
    private final JSpinner positionDelay_;
    private final JCheckBox useTimepointsCB_;
+   private final JCheckBox useAutofocusCB_;
    private final JPanel leftColumnPanel_;
    private final JPanel centerColumnPanel_;
    private final MMFrame sliceFrameAdvanced_;
@@ -701,6 +703,20 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
          }
       });
       
+      // checkbox to signal that autofocus should be used during acquisition
+      // another orphan UI element
+      useAutofocusCB_ = new JCheckBox("Autofocus");
+      useAutofocusCB_.setSelected(prefs_.getBoolean(panelName_, 
+              Properties.Keys.PLUGIN_ACQUSITION_USE_AUTOFOCUS, false));
+      useAutofocusCB_.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            prefs_.putBoolean(panelName_, 
+                    Properties.Keys.PLUGIN_ACQUSITION_USE_AUTOFOCUS, 
+                    useAutofocusCB_.isSelected());
+           }
+      });
+      
       
       // set up tabbed panels for GUI
       // make 3 columns as own JPanels to get vertical space right
@@ -730,6 +746,7 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
       centerColumnPanel_.add(positionPanel, "growx, wrap");
       centerColumnPanel_.add(multiChannelPanel_, "wrap");
       centerColumnPanel_.add(navigationJoysticksCB_, "wrap");
+      centerColumnPanel_.add(useAutofocusCB_, "wrap");
       
       // add the column panels to the main panel
       this.add(leftColumnPanel_);
@@ -739,8 +756,6 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
       // properly initialize the advanced slice timing
       advancedSliceTimingCB_.addItemListener(sliceTimingDisableGUIInputs);
       sliceTimingDisableGUIInputs.itemStateChanged(null);
-      //advancedSliceTimingCB_.doClick();
-      //advancedSliceTimingCB_.doClick();
       advancedSliceTimingCB_.addActionListener(showAdvancedTimingFrame);
       
       updateDurationLabels();
@@ -1590,6 +1605,16 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
          return false;
       }
       
+      // Autofocus settings
+      boolean useAutofocus = useAutofocusCB_.isSelected();
+      boolean autofocusAtT0 = prefs_.getBoolean(MyStrings.PanelNames.AUTOFOCUS.toString(), 
+              Properties.Keys.PLUGIN_AUTOFOCUS_ACQBEFORESTART, false);
+      int autofocusEachNFrames = prefs_.getInt(MyStrings.PanelNames.AUTOFOCUS.toString(), 
+              Properties.Keys.PLUGIN_AUTOFOCUS_EACHNIMAGES, 10);
+      String autofocusChannel = props_.getPropValueString(Devices.Keys.PLUGIN,
+            Properties.Keys.PLUGIN_AUTOFOCUS_CHANNEL);
+      
+      
       // it appears the circular buffer, which is used by both cameras, can only have one 
       // image size setting => we require same image height and width for second camera if two-sided
       if (twoSided) {
@@ -1684,7 +1709,7 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
                core_.setExposure(secondCamera, exposureTime);
             }
             
-            // Use this to build metadata for MVR plugin
+            // Use this to build metadata for MultiViewRegistration plugin
             String viewString = "";
             final String SEPARATOR = "_";
             // set up channels (side A/B is treated as channel too)
@@ -1742,7 +1767,7 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
                     actualVolumeDurationLabel_.getText());
             gui_.setAcquisitionProperty(acqName, "SPIMmode", spimMode.toString()); 
             // Multi-page TIFF saving code wants this one:
-            // TODO: support other types than GRAY16
+            // TODO: support other types than GRAY16  (NS: Why?? Cameras are all 16-bits, so not much reason for anything else
             gui_.setAcquisitionProperty(acqName, "PixelType", "GRAY16");
             gui_.setAcquisitionProperty(acqName, "z-step_um",  
                   NumberUtils.doubleToDisplayString(getStepSizeUm()) );
@@ -1769,7 +1794,7 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
             sink.start();
 
             // Loop over all the times we trigger the controller's acquisition
-            // For hardware-timed timepoints then we only trigger the controller once
+            // For hardware-timed timepoints we only trigger the controller once
             long acqStart = System.currentTimeMillis();
             for (int timePoint = 0; timePoint < nrFrames; timePoint++) {
                
@@ -1787,6 +1812,33 @@ public class AcquisitionPanel extends ListeningJPanel implements DevicesListener
                // check for stop button before each time point
                if (cancelAcquisition_.get()) {
                   throw new IllegalMonitorStateException("User stopped the acquisition");
+               }
+               
+               // this is where we autofocus if requested
+               if (useAutofocus) {
+                  // Note that we will not autofocus as expected when using hardware
+                  // timing.  Seems OK, since hardware timing will result in short
+                  // acquisition times that do not need autofocus
+                  if ( (autofocusAtT0 && timePoint == 0) || ( (timePoint > 0) && 
+                          (timePoint % autofocusEachNFrames == 0 ) ) ) {
+                     multiChannelPanel_.selectChannel(autofocusChannel);
+                     if (twoSided) {
+                        double score = autofocus_.runFocus(this, Devices.Sides.A, false,
+                                sliceTiming_, false);
+                        // TODO: apply new slice setting!
+                        score = autofocus_.runFocus(this, Devices.Sides.B, false,
+                                sliceTiming_, false);
+                     } else {
+                        Sides side = Devices.Sides.B;
+                        if (firstSideA) {
+                           side = Devices.Sides.A;
+                        }
+                        double score = autofocus_.runFocus(this, side, false,
+                                sliceTiming_, false);
+                     }
+                     // Restore settings of the controller
+                     prepareControllerForAquisition(hardwareTimepoints);
+                  }
                }
 
                numTimePointsDone_++;

plugins/ASIdiSPIM/src/org/micromanager/asidispim/AutofocusPanel.java
@@ -135,7 +135,13 @@ public class AutofocusPanel extends ListeningJPanel{
                     (String) fitFunctionSelection.getSelectedItem());
          }
       });
-      optionsPanel_.add(fitFunctionSelection);
+      optionsPanel_.add(fitFunctionSelection, "wrap");
+            
+      optionsPanel_.add(new JLabel("Minimum R²:"));
+      final JSpinner minimumR2Spinner = pu.makeSpinnerFloat(0.0, 1.0, 0.01,
+              Devices.Keys.PLUGIN, Properties.Keys.PLUGIN_AUTOFOCUS_MINIMUMR2, 0.75);
+      optionsPanel_.add(minimumR2Spinner);
+      
       // end options subpanel
       
       // start acquisiion optoinspanel
@@ -144,15 +150,15 @@ public class AutofocusPanel extends ListeningJPanel{
             "[right]16[center]",
             "[]8[]"));
       acqOptionsPanel_.setBorder(PanelUtils.makeTitledBorder(
-              "Acquisition AF Options"));
+              "Autofocus Options during Acquisition"));
       
       // whether or not to run autofocus at the start of the acquisition
-      final JCheckBox beforeStartCheckBox = pu.makeCheckBox("Run AF before starting acuiqistion",
+      final JCheckBox beforeStartCheckBox = pu.makeCheckBox("Autofocus before starting acquisition",
               Properties.Keys.PLUGIN_AUTOFOCUS_ACQBEFORESTART, panelName_, false);     
       acqOptionsPanel_.add(beforeStartCheckBox, "center, span 3, wrap");
       
       // autofocus every nth image
-      acqOptionsPanel_.add(new JLabel("Use every "));
+      acqOptionsPanel_.add(new JLabel("Autofocus every "));
       final JSpinner eachNTimePointsSpinner = pu.makeSpinnerInteger(1, 1000,
             Devices.Keys.PLUGIN,
             Properties.Keys.PLUGIN_AUTOFOCUS_EACHNIMAGES, 10);
@@ -167,12 +173,8 @@ public class AutofocusPanel extends ListeningJPanel{
       final JComboBox channelSelect = pu.makeDropDownBox(channels.toArray(), 
               Devices.Keys.PLUGIN, Properties.Keys.PLUGIN_AUTOFOCUS_CHANNEL, "");
       acqOptionsPanel_.add(new JLabel("Autofocus Channel: "));
-      acqOptionsPanel_.add(channelSelect);
-      
-      acqOptionsPanel_.add(new JLabel("Minimum R²:"));
-      final JSpinner minimumR2Spinner = pu.makeSpinnerFloat(0.0, 1.0, 0.01,
-              Devices.Keys.PLUGIN, Properties.Keys.PLUGIN_AUTOFOCUS_MINIMUMR2, 0.75);
-      acqOptionsPanel_.add(minimumR2Spinner);
+      acqOptionsPanel_.add(channelSelect, "wrap");
+
       
       // construct the main panel
       add(optionsPanel_);

plugins/ASIdiSPIM/src/org/micromanager/asidispim/Data/Properties.java
@@ -184,6 +184,7 @@ public class Properties {
       PLUGIN_DESIRED_SLICE_PERIOD("DesiredSlicePeriod"),
       PREFS_MINIMIZE_SLICE_PERIOD("MinimizeSlicePeriod"),
       PLUGIN_ACQUSITION_MODE("AcquisitionMode"),
+      PLUGIN_ACQUSITION_USE_AUTOFOCUS("UseAutofocusInAcquisition"),
       PLUGIN_CAMERA_MODE("CameraMode"),
       PREFS_ENABLE_POSITION_UPDATES("EnablePositionUpdates"),
       PREFS_ENABLE_ILLUM_PIEZO_HOME("EnableIllumPiezoHome"),

plugins/ASIdiSPIM/src/org/micromanager/asidispim/MultiChannelSubPanel.java
@@ -354,18 +354,26 @@ public class MultiChannelSubPanel extends ListeningJPanel {
    }
    
    /**
+    * Moves the hardware to the state defined in the channel
+    * @param channel should be a channel in the current channel group
+    */
+   public void selectChannel(String channel) {
+       try {
+         core_.setConfig(channelGroup_.getSelectedItem().toString(), channel);
+         core_.waitForConfig(channelGroup_.getSelectedItem().toString(), channel);
+      } catch (Exception e) {
+         MyDialogUtils.showError(e, "Couldn't select preset " + channel +
+               "of channel group " + channelGroup_.getSelectedItem().toString());
+      }
+   }
+   
+   /**
     * Takes care of actually selecting next channel in table.
     * Called by acquisition code.  Blocks until devices ready.
     */
    public void selectNextChannel() {
       ChannelSpec channel = usedChannels_[nextChannelIndex_];
-      try {
-         core_.setConfig(channelGroup_.getSelectedItem().toString(), channel.config_);
-         core_.waitForConfig(channelGroup_.getSelectedItem().toString(), channel.config_);
-      } catch (Exception e) {
-         MyDialogUtils.showError(e, "Couldn't select preset " + channel.config_ +
-               "of channel group " + channelGroup_.getSelectedItem().toString());
-      }
+      selectChannel(channel.config_);
       nextChannelIndex_++;
       if (nextChannelIndex_ == usedChannels_.length) {
          nextChannelIndex_ = 0;

plugins/ASIdiSPIM/src/org/micromanager/asidispim/Utils/AutofocusUtils.java
@@ -86,8 +86,10 @@ public class AutofocusUtils {
    /**
     * Acquires image stack by scanning the mirror, calculates focus scores
     * Acquires around the current piezo position. As a side effect, will
-    * temporarily set the center position to the current position (but restore
-    * the center position on exit).
+    * temporarily set the center position to the current position.  If the 
+    * autofocus is successful (based on the goodness of fit of the focus curve),
+    * the scanner slice position will  be set to the in-focus position, otherwise
+    * it will be returned to the original position
     *
     * @param caller - calling panel, used to restore settings after focusing
     * @param side - A or B
@@ -95,7 +97,7 @@ public class AutofocusUtils {
     *                            around the imaging center set in the setup panel (false)
     * @param sliceTiming - Data structure with current device timing setting
     * @param runAsynchronously - whether or not to run the function asynchronously
-    * @return galvo slice position with best focus
+    * @return R-square for Gaussian fit to the focus data
     *
     */
    public double runFocus(
@@ -111,6 +113,8 @@ public class AutofocusUtils {
          protected Double doInBackground() throws Exception {
 
             double bestGalvoPosition = 0;
+            
+            double r2 = 0;
 
             if (gui_.getAutofocus() == null) {
                throw new ASIdiSPIMException("Please define an autofocus methods first");
@@ -154,6 +158,8 @@ public class AutofocusUtils {
             final float imagingCenter = prefs_.getFloat(
                     MyStrings.PanelNames.SETUP.toString() + side.toString(),
                     Properties.Keys.PLUGIN_PIEZO_CENTER_POS, 0);
+            final float minimumRSquare =  props_.getPropValueFloat(Devices.Keys.PLUGIN,
+                     Properties.Keys.PLUGIN_AUTOFOCUS_MINIMUMR2, null);
             final double originalPiezoPosition = positions_.getUpdatedPosition(piezoDevice);
             final double originalGalvoPosition = positions_.getUpdatedPosition(galvoDevice);
             final double piezoCenter = centerAtCurrentZ ? originalPiezoPosition : imagingCenter;
@@ -333,7 +339,7 @@ public class AutofocusUtils {
                highestIndex = Fitter.getIndex(scoresToPlot[0], bestGalvoPosition);
                scoresToPlot[1] = Fitter.getFittedSeries(scoresToPlot[0], 
                        function, fitParms);
-               double r2 = Fitter.getRSquare(scoresToPlot[0], function, fitParms);
+               r2 = Fitter.getRSquare(scoresToPlot[0], function, fitParms);
                
                // display the best scoring image in the debug stack if it exists
                // or if not then in the snap/live window if it exists
@@ -386,7 +392,8 @@ public class AutofocusUtils {
                   
                   // move galvo to maximal focus position if we found one
                   // if for some reason we were unsuccessful then restore to original position
-                  if (highestIndex >= 0) {
+                  // TODO: 
+                  if (highestIndex >= 0 && r2 > minimumRSquare) {
                      positions_.setPosition(galvoDevice, Directions.Y, bestGalvoPosition);
                   } else {
                      positions_.setPosition(galvoDevice, Directions.Y, originalGalvoPosition);
@@ -407,7 +414,7 @@ public class AutofocusUtils {
                   throw new ASIdiSPIMException(ex, "Error while restoring hardware state after autofocus.");
                }
             }
-            return bestGalvoPosition;
+            return r2;
          }
 
          @Override

plugins/ASIdiSPIM/src/org/micromanager/asidispim/Utils/PanelUtils.java
@@ -70,6 +70,11 @@ public class PanelUtils {
    /**
     * makes JSlider for double values where the values are multiplied by a scale factor
     * before internal representation as integer (as JSlider requires)
+    * @param min
+    * @param max
+    * @param scalefactor
+    * @param devKey
+    * @param propKey
     * @return
     */
    public JSlider makeSlider(double min, double max, final int scalefactor,
@@ -130,8 +135,8 @@ public class PanelUtils {
       
       //Create the label table
       Hashtable<Integer, JLabel> labelTable = new Hashtable<Integer, JLabel>();
-      labelTable.put( new Integer(intmax), new JLabel(Double.toString(max)) );
-      labelTable.put( new Integer(intmin), new JLabel(Double.toString(min)) );
+      labelTable.put(intmax, new JLabel(Double.toString(max)) );
+      labelTable.put(intmin, new JLabel(Double.toString(min)) );
       
       js.setLabelTable( labelTable );
       js.setPaintTicks(true);
@@ -146,11 +151,9 @@ public class PanelUtils {
     * that were generated by changes in the device to be propagated back to the UI.
     * @param min - minimum value for the spinner
     * @param max - maximum value for the spinner
-    * @param step - stepsize for the spinner
-    * @param props - the singleton properties class instance that holds all our property info
-    * @param devs - singleton device class instance holding device info
-    * @param devKeys - array of device keys, use inline constructor "new Devices.Keys[]{<list of devices>}"
+    * @param devKeys - array of device keys, use inline constructor "new Devices.Keys[]{list of devices}"
     * @param propKey - property key for this spinner
+    * @param defaultVal - initial value
     * @return the created JSpinner
     */
    public JSpinner makeSpinnerInteger(int min, int max, 
@@ -236,11 +239,9 @@ public class PanelUtils {
     * that were generated by changes in the device to be propagated back to the UI.
     * @param min - minimum value for the spinner
     * @param max - maximum value for the spinner
-    * @param step - stepsize for the spinner
-    * @param props - the singleton properties class instance that holds all our property info
-    * @param devs - singleton device class instance holding device info
     * @param devKey - device keys
     * @param propKey - property key for this spinner
+    * @param defaultVal - initial value
     * @return the created JSpinner
     */
    public JSpinner makeSpinnerInteger(int min, int max, 
@@ -267,8 +268,8 @@ public class PanelUtils {
    
    /**
     * Utility function to write to the spinner value.
-    * @param sp
-    * @return
+    * @param sp - spinning that should be set
+    * @param f - new value for the spinner
     */
    public static void setSpinnerFloatValue(JSpinner sp, float f) {
       // TODO figure out why the type of value in the numbermodel is 
@@ -283,8 +284,8 @@ public class PanelUtils {
    
    /**
     * Utility function to write to the spinner value.
-    * @param sp
-    * @return
+    * @param sp spinner whole value should be set
+    * @param d new value for the spinner
     */
    public static void setSpinnerFloatValue(JSpinner sp, double d) {
       // TODO figure out why the type of value in the numbermodel is 
@@ -303,9 +304,7 @@ public class PanelUtils {
     * @param min - minimum value for the spinner
     * @param max - maximum value for the spinner
     * @param step - stepsize for the spinner
-    * @param props - the singleton properties class instance that holds all our property info
-    * @param devs - singleton device class instance holding device info
-    * @param devKeys - array of device keys, use inline constructor "new Devices.Keys[]{<list of devices>}"
+    * @param devKeys - array of device keys, use inline constructor "new Devices.Keys[]{list of devices}"
     * @param propKey - property key for this spinner
     * @param defaultVal - 
     * @return the created JSpinner
@@ -394,8 +393,6 @@ public class PanelUtils {
     * @param min - minimum value for the spinner
     * @param max - maximum value for the spinner
     * @param step - stepsize for the spinner
-    * @param props - the singleton properties class instance that holds all our property info
-    * @param devs - singleton device class instance holding device info
     * @param devKey - device key
     * @param propKey - property key for this spinner
     * @param defaultVal - 
@@ -484,8 +481,6 @@ public class PanelUtils {
     * Sets selection based on property value and attaches a Listener.
     * 
     * @param propKey - property key as known in the params
-    * @param props
-    * @param devs
     * @param defaultVal
     * @param vals - array of strings, each one is a different option in the dropdown 
     * @param devKey

commit 88c8499e1ce0e86df595e57a53c8d117ca9a2929

ASIdiSPIM: correct probably bug with pausing position updates introduced in r15471
git-svn-id: https://valelab4.ucsf.edu/svn/micromanager2/trunk@16276 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

plugins/ASIdiSPIM/src/org/micromanager/asidispim/Data/Positions.java
@@ -352,8 +352,6 @@ public class Positions {
       }
    }
 
-   
-   
    /**
     * updates the cached stage positions via calls to MMCore
     */
@@ -419,5 +417,4 @@ public class Positions {
       return "";
    }
 
-   
 }

plugins/ASIdiSPIM/src/org/micromanager/asidispim/Utils/StagePositionUpdater.java
@@ -30,6 +30,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import org.micromanager.asidispim.Data.Devices;
 import org.micromanager.asidispim.Data.Positions;
 import org.micromanager.asidispim.Data.Properties;
+import org.micromanager.utils.ReportingUtils;
 
 /**
  *
@@ -146,10 +147,11 @@ public class StagePositionUpdater {
     * Call this with true to temporarily turn off updates.
     * Be sure to call it again with false. Keeps an internal counter
     * so that the calling code does not need to know whether other code
-    * also called the pauseUpdate function.
+    * also called the pauseUpdate function. If an update cycle is underway
+    * when called with true the update cycle will finish.
     * @param pause true disables updates temporarily, and increases the pause 
-    * counter.  false decreases the pause counter.  When the counter returns to 
-    * 0, unpauses the updater
+    * counter.  false decreases the pause counter.  When the counter returns 
+    * to 0, unpauses the updater
     */
    public void pauseUpdates(boolean pause) {
       if (pause) {
@@ -161,8 +163,12 @@ public class StagePositionUpdater {
             pauseCounter_ = 0;
          }
       }
-      if (pauseCounter_ < 2) {
-         pauseUpdates_.set(pause);
+      if (pauseCounter_ == 0) {
+         pauseUpdates_.set(false);
+         ReportingUtils.logDebugMessage("Unpaused position updates");
+      } else {
+         pauseUpdates_.set(true);
+         ReportingUtils.logDebugMessage("Paused position updates");
          for (ListeningJPanel panel : panels_) {
             panel.stoppedStagePositions();
          }