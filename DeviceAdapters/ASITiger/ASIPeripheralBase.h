kasbah/micromanager

commit fbd077fb5f8c56599f9576e5472eb315cf844d95

ASITiger: set up infrastructure for shared properties and implemented it on a single scanner property to test.  Also cleaned up Shutdown() calls and SetSpotInterval() for scanner.
Here is the approach taken to share properties between devices (some devices are on same card and some properties are really per-card instead of per-axis or per-device)
 1. peripheral devices register with hub during Initialize() by calling RegisterPeripheral() with device label and Tiger address
 2. the hub adds that pair to a lookup table of all the devices that share the same card
 3. when a shared property changes in a device, it notifies the hub with the card address, the name of the property that was changed, and the new property value
 4. the hub sets this property in all devices sharing the same card
    a. the hub sets a flag which changes the behavior of the property action handler
      i. the action handler is the only place we have the pointer to the PropertyBase object that we call pProp->Set() on
      ii. the MM::AfterSet branch of the property action handler is modified to communicate with the hardware only if the flag is unset
    b. the hub calls the core's SetDeviceProperty() for the specified property name for all devices which share the same card according to its lookup table
    c. the hub unsets the flag
 
This required modifying the following sections of code:
1. the hub device
  a. add methods to maintain a record of registered peripheral devices
  b. add method which will call SetProperty() on appropriate peripheral devices on demand
2. the peripheral Initialize()/Shutdown() to register/unregister with the hub
3. the action handler for shared properties, on the MM::AfterSet branch
  a. if the flag is true, nothing needs to be done
  b. if the flag is false, do the normal communication over serial to set value in hardware and then notify hub

git-svn-id: https://valelab4.ucsf.edu/svn/micromanager2/trunk@16463 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

DeviceAdapters/ASITiger/ASIPeripheralBase.h
@@ -41,6 +41,7 @@ public:
       ASIBase<TDeviceBase, UConcreteDevice>(name),
       hub_(NULL),
       addressString_(g_EmptyCardAddressStr),
+      addressChar_(g_EmptyCardAddressChar),
       refreshProps_(false),
       refreshOverride_(false)
    {
@@ -57,10 +58,20 @@ public:
       }
    }
 
+   int Shutdown() {
+      char deviceLabel[MM::MaxStrLength];
+      this->GetLabel(deviceLabel);
+      string str(deviceLabel);
+      hub_->UnRegisterPeripheral(str);
+      return ASIBase::Shutdown();
+   }
+
    // The Initialize() of child (concrete peripheral) classes must call this.
    // Note that initialize_ is set by the concrete child class.
    int PeripheralInitialize(bool skipFirmware = false)
    {
+      ostringstream command; command.str("");
+
       // get the hub information
       MM::Hub* genericHub = this->GetParentHub();
       if (!genericHub)
@@ -75,7 +86,6 @@ public:
       //    FWheel: different serial terminator and command set => own Initialize() handles
       if (!skipFirmware)
       {
-         ostringstream command; command.str("");
          command << addressChar_ << "V";
          RETURN_ON_MM_ERROR ( hub_->QueryCommandVerify(command.str(),":A v") );
          RETURN_ON_MM_ERROR ( hub_->ParseAnswerAfterPosition(4, this->firmwareVersion_ ));
@@ -99,6 +109,12 @@ public:
          RETURN_ON_MM_ERROR ( this->CreateProperty(g_FirmwareBuildPropertyName, this->firmwareBuild_.c_str(), MM::String, true) );
       }
 
+      // register peripheral with the hub, get label in c-string and convert to c++ string
+      char deviceLabel[MM::MaxStrLength];
+      this->GetLabel(deviceLabel);
+      string str(deviceLabel);
+      hub_->RegisterPeripheral(str, addressChar_);
+
       // I can't seem to define action handlers here that will apply to derived classes
       // I'd like to do that to avoid so much boilerplate in separate device code
       // problem is likely my misunderstanding of inheritance
@@ -117,10 +133,8 @@ public:
 
 protected:
    ASIHub *hub_;           // pointer to hub object used for serial communication
-   string addressString_;  // address within hub, in hex format
-                           // generally two characters (e.g. '31') but could be four characters (e.g. '3132') if device axes are split between cards
-   string addressChar_;    // address within hub, in single character (possibly extended ASCII)
-                           // in case of XY device, the MM device is split across two HW cards so it will be two characters
+   string addressString_;  // address within hub, in hex format, should be two characters (e.g. '31')
+   string addressChar_;    // address within hub, in single character (allowed to be extended ASCII so use string to store)
    bool refreshProps_;     // true when property values should be read anew from controller each time
    bool refreshOverride_;  // true when device wants to manually force refreshes temporarily
 

DeviceAdapters/ASITiger/ASIBase.h
@@ -64,13 +64,15 @@ public:
       this->InitializeDefaultErrorMessages();
       InitializeASIErrorMessages();
 
+      // note that this name is different from the label that MM uses in e.g. SetProperty()
+
       // name property will be used to re-create the object by calling CreateDevice again with this parameter
       // if name isn't specified then skip this step (=> method for parent objects to delay setting name until child created)
       if (strcmp(name, "") != 0)
          this->CreateProperty(MM::g_Keyword_Name, name, MM::String, true);
    }
 
-   virtual ~ASIBase() { Shutdown(); }
+   virtual ~ASIBase() { }
 
    int Shutdown()
    {

DeviceAdapters/ASITiger/ASICRISP.h
@@ -33,7 +33,7 @@ class CCRISP : public ASIPeripheralBase<CAutoFocusBase, CCRISP>
 {
 public:
    CCRISP(const char* name);
-   ~CCRISP() { Shutdown(); }
+   ~CCRISP() { }
   
    // Device API
    // ----------

DeviceAdapters/ASITiger/ASIClocked.h
@@ -35,8 +35,8 @@ class CClocked : public ASIPeripheralBase<CStateDeviceBase, CClocked>
 {
 public:
    CClocked(const char* name);
-   ~CClocked() { Shutdown(); }
-  
+   ~CClocked() { }
+
    // Generic device API
    // ----------
    int Initialize();

DeviceAdapters/ASITiger/ASIFWheel.h
@@ -35,7 +35,7 @@ class CFWheel : public ASIPeripheralBase<CStateDeviceBase, CFWheel>
 {
 public:
    CFWheel(const char* name);
-   ~CFWheel() { Shutdown(); }
+   ~CFWheel() { }
   
    // Generic device API
    // ----------

DeviceAdapters/ASITiger/ASIHub.cpp
@@ -50,7 +50,8 @@ ASIHub::ASIHub() :
       serialTerminator_(g_SerialTerminatorDefault),
       serialRepeatDuration_(0),
       serialRepeatPeriod_(500),
-      serialOnlySendChanged_(true)
+      serialOnlySendChanged_(true),
+      updatingSharedProperties_(false)
 {
    CPropertyAction* pAct = new CPropertyAction(this, &ASIHub::OnPort);
    CreateProperty(MM::g_Keyword_Port, "Undefined", MM::String, false, pAct, true);
@@ -472,6 +473,21 @@ string ASIHub::GetDefineString(const build_info_type build, const string substri
       }
    }
    return "";
+}
+
+int ASIHub::UpdateSharedProperties(string addressChar, string propName, string value) {
+   int ret = DEVICE_OK;
+   updatingSharedProperties_ = true;
+   for (map<string,string>::iterator it=deviceMap_.begin(); it!=deviceMap_.end(); ++it) {
+      if (addressChar == it->second) {
+         int ret_last = GetCoreCallback()->SetDeviceProperty(it->first.c_str(), propName.c_str(), value.c_str()) != DEVICE_OK;
+         if (ret_last!=DEVICE_OK) {
+            ret = ret_last;
+         }
+      }
+   }
+   updatingSharedProperties_ = false;
+   return ret;
 }
 
 int ASIHub::OnPort(MM::PropertyBase* pProp, MM::ActionType eAct)

DeviceAdapters/ASITiger/ASIHub.h
@@ -118,6 +118,18 @@ public:
    // get define string from substring (e.g. the RING BUFFER define has the # of positions)
    string GetDefineString(const build_info_type build, const string substringToLookFor);
 
+   void RegisterPeripheral(const string deviceLabel, const string addressChar) {
+      deviceMap_[deviceLabel] = addressChar;  // add device to lookup table
+   }
+
+   void UnRegisterPeripheral(const string deviceLabel) {
+      deviceMap_.erase(deviceLabel);  // remove device from lookup table
+   }
+
+   bool UpdatingSharedProperties() { return updatingSharedProperties_; }
+
+   int UpdateSharedProperties(string addressChar, string propName, string value);
+
    // action/property handlers
    int OnPort                       (MM::PropertyBase* pProp, MM::ActionType eAct);
    int OnSerialTerminator           (MM::PropertyBase* pProp, MM::ActionType eAct);
@@ -145,6 +157,10 @@ private:
    long serialRepeatPeriod_;  // how often in ms the command is sent
    bool serialOnlySendChanged_;        // if true the serial command is only sent when it has changed
    MMThreadLock threadLock_;  // used to lock thread during serial transaction
+   bool updatingSharedProperties_;
+   map<string, string> deviceMap_;  // to implement properties shared between devices
+        // key is the device name, value is the Tiger address (normally a single character, see note about addressChar_ in ASIPeripheralBase)
+
 };
 
 

DeviceAdapters/ASITiger/ASILED.h
@@ -33,7 +33,7 @@ class CLED : public ASIPeripheralBase<CShutterBase, CLED>
 {
 public:
    CLED(const char* name);
-   ~CLED() { Shutdown(); }
+   ~CLED() { }
   
    // Device API
    // ----------

commit 2dbf56fdf03cb624448b5577fcecbd36bcb28ca6

ASITiger: add null check that was causing crashes on unsuccessful loads of Tiger devices (bug introduced in r16463 with properties shared between devices)
git-svn-id: https://valelab4.ucsf.edu/svn/micromanager2/trunk@16643 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

DeviceAdapters/ASITiger/ASIPeripheralBase.h
@@ -63,7 +63,9 @@ public:
       char deviceLabel[MM::MaxStrLength];
       this->GetLabel(deviceLabel);
       string str(deviceLabel);
-      hub_->UnRegisterPeripheral(str);
+      if (hub_) {
+         hub_->UnRegisterPeripheral(str);
+      }
       return (ASIBase<TDeviceBase, UConcreteDevice>::Shutdown());
    }
 